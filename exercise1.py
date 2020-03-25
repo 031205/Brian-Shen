f4974bdd2b24b3da60f9cfa4a849f7230a5d2835
031205 <62634319+031205@users.noreply.github.com>
2020-03-25 13:01:17 -0400

    Add files via upload

---
+++ exercise1.py
@@ -0,0 +1,10 @@
+Name = 'Brian Shen'
+Address = '16 Parklawn Crescent'
+CityandState = 'Thornhill ON'
+Zipcode = 'L3T 6X1'
+Country = 'Canada'
+print(Name)
+print(Address)
+print(CityandState,Zipcode)
+print(Country)
+
